2a107b4a7cca8b10e01b576e3a0f35d5551e724b
Kevin Frei <kevinfrei@hotmail.com>
2026-09-25 13:35:25 -0700

    Fix Colin's Typos

--- Sixteen750/src/main/java/org/firstinspires/ftc/teamcode/sixteen750/pedro/meepmeep/Paths.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/teamcode/sixteen750/pedro/meepmeep/Paths.java
@@ -4,13 +4,14 @@ import static com.pedropathing.api.Paths.*;
 
 import com.bylazar.configurables.annotations.Configurable;
 import com.pedropathing.follower.Follower;
-import com.pedropathing.paths.curves.bezier.BezierCurve;
-import com.pedropathing.paths.curves.Line;
-import com.pedropathing.paths.interpolator.Interpolator;
 import com.pedropathing.paths.Path;
-import com.pedropathing.paths.
+import com.pedropathing.paths.curves.Line;
+import com.pedropathing.paths.curves.bezier.BezierCurve;
+import com.pedropathing.paths.interpolator.Interpolator;
+
 @Configurable
 public class Paths {
+
     Poses.StartPoses sp = new Poses.StartPoses();
     Poses.Push12ParkPoses p = new Poses.Push12ParkPoses();
 
@@ -19,26 +20,30 @@ public class Paths {
     public Path GardenPreIntToGardenInt;
     public Path GardenIntToLaunch2;
     public Path Launch2ToPark;
+
     public Path StartToPartnerPark() {
         return line(sp.Start, p.PartnerPark).constant(p.PartnerParkHead);
     }
+
     public Path PartnerParkToLaunch1() {
-        return line(p.PartnerPark, p.Launch1).linear(p.PartnerParkHead,p.Launch1Head);
+        return line(p.PartnerPark, p.Launch1).linear(p.PartnerParkHead, p.Launch1Head);
     }
+
     public Path Launch1ToGardenPreInt() {
         return line(p.Launch1, p.GardenPreInt).constant(p.GardenIntHead);
     }
+
     public Path GardenPreIntToGardenInt() {
         return line(p.GardenPreInt, p.GardenInt).constant(p.GardenIntHead);
     }
+
     public Path GardenIntToLaunch2() {
-        return curve(p.GardenInt, p.Launch2Ctrl1, p.Launch2Ctrl2, p.Launch2).constant(p.Launch2Head);
+        return curve(p.GardenInt, p.Launch2Ctrl1, p.Launch2Ctrl2, p.Launch2).constant(
+            p.Launch2Head
+        );
     }
+
     public Path Launch2ToPark() {
         return line(p.Launch2, p.Park).constant(p.ParkHead);
     }
-
-
-
-
 }
--- Sixteen750/src/main/java/org/firstinspires/ftc/teamcode/sixteen750/pedro/meepmeep/Poses.java
+++ Sixteen750/src/main/java/org/firstinspires/ftc/teamcode/sixteen750/pedro/meepmeep/Poses.java
@@ -3,33 +3,37 @@ package org.firstinspires.ftc.teamcode.sixteen750.pedro.meepmeep;
 import com.bylazar.configurables.annotations.Configurable;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.math.Pose;
-@Configiurable
+
+@Configurable
 public class Poses {
+
     public static Follower follower;
 
     @Configurable
     public static class StartPoses {
-        public static Pose Start = new Pose(7,64);
+
+        public static Pose Start = new Pose(7, 64);
+
         public static Pose getStart() {
             return new Pose(7, 64, Math.toRadians(0));
         }
     }
+
     @Configurable
     public static class Push12ParkPoses {
-        public static Pose PartnerPark = new Pose(11,83);
+
+        public static Pose PartnerPark = new Pose(11, 83);
         public static double PartnerParkHead = Math.toRadians(0);
-        public static Pose Launch1 = new Pose(56,18);
+        public static Pose Launch1 = new Pose(56, 18);
         public static double Launch1Head = Math.toRadians(90);
-        public static Pose GardenPreInt = new Pose(7,22);
-        public static Pose GardenInt = new Pose(7,10.5);
+        public static Pose GardenPreInt = new Pose(7, 22);
+        public static Pose GardenInt = new Pose(7, 10.5);
         public static double GardenIntHead = Math.toRadians(270);
-        public static Pose Launch2 = new Pose(56,125);
+        public static Pose Launch2 = new Pose(56, 125);
         public static double Launch2Head = Math.toRadians(270);
-        public static Pose Launch2Ctrl1 = new Pose(48,24);
-        public static Pose Launch2Ctrl2 = new Pose(22,132);
-        public static Pose Park = new Pose(18,120);
+        public static Pose Launch2Ctrl1 = new Pose(48, 24);
+        public static Pose Launch2Ctrl2 = new Pose(22, 132);
+        public static Pose Park = new Pose(18, 120);
         public static double ParkHead = Math.toRadians(180);
-
-
     }
 }
